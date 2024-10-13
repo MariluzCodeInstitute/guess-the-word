43be7fdcfc5bb7ec3216274c5ba15cfff5ea4cab
Mariluz Herrera <gcsf2mk0@students.codeinstitute.net>
2024-10-13 15:20:17 +0000

    Create function to display the welcome message and instructions to the console

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def display_intro():
+    """
+    Diplays the welcome message and instructions for the game    
+    """
+    print("Welcome to Guess The Word!\n")
+    print("You have 6 attempts to guess a 5-character-long word\n")
+    print(
+        "On each attempt, if any of letters are in the right position, "
+        "they will be green")
+    print(
+        "If any of the letters are present in the word but not in the right position, "
+        "they will be yellow"
+    )
+    print("If the letter is not in the word, it will not be coloured\n")
+    print("Good luck!")
+
+display_intro()
+
