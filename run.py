c43b8d9da220a0132dfbc17ac086f6c74051ea2c
Mariluz Herrera <gcsf2mk0@students.codeinstitute.net>
2024-10-13 20:23:44 +0000

    Remove Word class and create a Game class with constructor and associated functions in it

--- run.py
+++ run.py
@@ -5,29 +5,75 @@ from nltk.corpus import words
 nltk.download('words')
 dictionary = set(words.words())
 
-MAX_ATTEMPTS = 6
-grid = [['_' for _ in range(5)] for _ in range(MAX_ATTEMPTS)]
-word_list = ['apple', 'baker', 'crane', 'delta', 'eagle']
-guess_list = []
-attempt = 0
+word_list = ['apple', 'baker', 'crane', 'delta', 'eagle'] # Small list for testing purposes
 
-class Word:
+class Game:
     """
-    Word class
+    Game class that handles target word choice, user input, validations, checks and feedback
     """
-    def __init__(self, target_word):
+    MAX_ATTEMPTS = 6 # This number will not change so it makes sense that is a constant
+
+    def __init__(self):
         self.target_word = random.choice(word_list)
+        self.grid = [['_' for _ in range(5)] for _ in range(self.MAX_ATTEMPTS)]
+        self.attempt = 0
+        self.guess_list = []
+    
+    def display_grid(self):
+        """
+        Function to display a 5x6 grid in the terminal
+        Code inspired from suggestions on StackOverflow:
+        https://stackoverflow.com/questions/77174842/how-to-print-a-grid-with-multiple-columns-in-the-terminal-using-for-loops
+        """
+        print("Guess The Word:\n")
 
-def display_grid():
-    """
-    Function to display a 5x6 grid in the terminal
-    Code inspired from suggestions on StackOverflow:
-    https://stackoverflow.com/questions/77174842/how-to-print-a-grid-with-multiple-columns-in-the-terminal-using-for-loops
-    """
-    print("Guess The Word:\n")
+        for row in self.grid:
+            print(' '.join(row))
+    
+    def get_player_input(self):
+        """
+        Asks the player to enter a word/guess and runs validations on it
+        """
+        player_guess = input("Enter a 5-letter word: ").strip().upper()
+        self.validate_input(player_guess)
 
-    for row in grid:
-        print(' '.join(row))
+    def validate_input(self, guess):
+        """
+        Validates player's input and returns feedback accordingly
+        """
+        if len(guess) != 5:
+            print(f"Your word must be a 5-letter word. You provided a {len(guess)}-letter word. Please try again")
+            self.get_player_input()
+        elif guess.isalpha() == False:
+            print("Your word must contain only letters. Please try again.")
+            self.get_player_input()
+        elif guess.lower() not in dictionary:
+            print(f"{guess} is not an English word. Please try again.")
+            self.get_player_input()
+        elif guess in self.guess_list:
+            print(f"You already tried the word {guess}. Please choose another word")
+            self.get_player_input()
+        else:
+            print("Valid word!")
+            self.guess_list.append(guess)
+            self.update_grid(guess)
+    
+    def update_grid(self, guess):
+        self.grid[self.attempt] = guess
+        self.attempt +=1
+        self.display_grid()
+
+        self.check_guess(guess)
+
+    def check_guess(self, guess):
+        if guess.lower() == self.target_word:
+            print("Congratulations! Your guess is right!")
+        else:
+            if self.attempt < self.MAX_ATTEMPTS:
+                self.get_player_input()
+            else:
+                print(f"Sorry, the word was {self.target_word}.")
+
 
 def display_intro():
     """
@@ -46,44 +92,8 @@ def display_intro():
     print("Good luck!")
 
 
-def get_player_input():
-    """
-    Asks the player to enter a word/guess and runs validations on it
-    """
-    player_guess = input("Enter a 5-letter word: ").strip().upper()
-    validate_input(player_guess)
-
-def validate_input(guess):
-    """
-    Validates player's input and returns feedback accordingly
-    """
-    if len(guess) != 5:
-        print(f"Your word must be a 5-letter word. You provided a {len(guess)}-letter word. Please try again")
-        get_player_input()
-    elif guess.isalpha() == False:
-        print("Your word must contain only letters. Please try again.")
-        get_player_input()
-    elif guess.lower() not in dictionary:
-        print(f"{guess} is not an English word. Please try again.")
-        get_player_input()
-    elif guess in guess_list:
-        print(f"You already tried the word {guess}. Please choose another word")
-        get_player_input()
-    else:
-        print("Valid word!")
-        guess_list.append(guess)
-        update_grid(guess)
-
-def update_grid(guess):
-    global attempt
-    grid[attempt] = guess
-    attempt +=1
-    display_grid()
-
-    if attempt < MAX_ATTEMPTS:
-        get_player_input()
-
 display_intro()
-display_grid()
-get_player_input()
+game = Game()
+game.display_grid()
+game.get_player_input()
 
